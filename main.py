97fe1eb0116175dc2f960c1cfc183d267046c4af
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-15 16:10:29 +0700

    display

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ arena_lebar = 500
 arena_tinggi = 500
 window = pygame.display.set_mode((arena_lebar,arena_tinggi))
 
+def draw_grid(surface):
+    jumlah_baris = 10
+    jumlah_kolom = 10
+    jarak_baris = arena_lebar // jumlah_baris
+    jarak_kolom = arena_tinggi // jumlah_kolom
+    window.fill((255,255,255))
+    for baris_ke in range(jumlah_baris):
+        x = jarak_baris*baris_ke
+        y = jarak_kolom*baris_ke
+        pygame.draw.line(surface,(0,0,0),(x,0),(x,arena_tinggi))
+        pygame.draw.line(surface,(0,0,0),(0,y),(arena_lebar,y))
+
+
 
 isRun = True
 while isRun:
@@ -14,7 +27,7 @@ while isRun:
         if event.type == pygame.QUIT:
             isRun = False
 
-    window.fill((255,255,255))
+    draw_grid(window)
     pygame.display.update()
 
 pygame.quit()            
