18afa16e9d65de55f2800feac3381395d6372238
Arya Wiratama <aryawiratama2401@gmail.com>
2025-02-15 15:58:36 +0700

    init

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import pygame
 # Init
 
 pygame.init()
-arena_lebar = 200
-arena_tinggi = 200
+arena_lebar = 500
+arena_tinggi = 500
+window = pygame.display.set_mode((arena_lebar,arena_tinggi))
 
-pygame.display.set_mode((arena_lebar,arena_tinggi))
 
 isRun = True
 while isRun:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRun = False
+
+    window.fill((255,255,255))
+    pygame.display.update()
+
+pygame.quit()            
 
 # Input
 
